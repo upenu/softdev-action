dc8a1767bbe64c2268b5eab4953ebe2feec82a33
Chase Norman <c_@berkeley.edu>
2021-08-24 23:09:19 -0700

    built

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,10 +1,9 @@
-import {wait} from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 import {expect, test} from '@jest/globals'
 
-test('throws invalid number', async () => {
+/*test('throws invalid number', async () => {
   const input = parseInt('foo', 10)
   await expect(wait(input)).rejects.toThrow('milliseconds not a number')
 })
@@ -26,4 +25,4 @@ test('test runs', () => {
     env: process.env
   }
   console.log(cp.execFileSync(np, [ip], options).toString())
-})
+})*/
